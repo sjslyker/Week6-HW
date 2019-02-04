3ab23dc3ec8c0fce2888e2d6ab76fbe369f3baf0
Steven S <stevenjslyker@gmail.com>
2019-02-03 21:07:19 -0700

    final.2

--- assets/javascript/hw6.js
+++ assets/javascript/hw6.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     
     function displayGifs(){
         var newAnimal = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + newAnimal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newAnimal + "&api_key=dc6zaTOxFJmzC&limit=10";
         console.log(queryURL); 
         $.ajax({
             url: queryURL,
